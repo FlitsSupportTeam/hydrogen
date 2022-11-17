06abc8698eaff9e941667c9d9a60ad20911e018d
Vaibhav <vaibhav@getflits.com>
2022-11-17 16:27:18 +0530

    small fixes

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     storeDomain: 'flits-plus.myshopify.com', // Oxygen.env.PUBLIC_STORE_DOMAIN
     storefrontToken: 'a63ff125050d5f859e2d2b21952ea1b2', // Oxygen.env.PUBLIC_STOREFRONT_API_TOKEN
-    privateStorefrontToken: Oxygen.env.PRIVATE_STOREFRONT_API_TOKEN,
+    privateStorefrontToken: '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',//Oxygen.env.PRIVATE_STOREFRONT_API_TOKEN
     storefrontApiVersion: '2022-07',
     storefrontId: Oxygen.env.PUBLIC_STOREFRONT_ID,
   },
